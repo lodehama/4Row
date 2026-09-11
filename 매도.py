14b9264cb837d48a301d4a6f02527dcc25ef8cca
lodehama <lodenx24@gmail.com>
2026-09-11 11:31:47 +0900

    Update 매도.py

--- 매도.py
+++ 매도.py
@@ -8,42 +8,40 @@ class SellStock:
 
     def sell(self):
         total_sell = 0
-        print('===== 현재가 =====')
-        a = 0
-        for i in self.stock_list :
-            a += 1
-            # print(f"{a}) {i}: {stock_list[i]:,}원 / 등락률 : {change}%"")  # 이거 하면 마지막 거만 나옴
-            print(f"{a}) {i}: {self.stock_list[i]:,}원 / 등락률 : {self.change_list[i]}%")
-        while True :
-            a = 0
-            stock_name = input('어떤 주식을 매도할까요?')
-            stock_number = input('몇 주 매도할까요?')
-            for i in self.stock_list :
-                if a+1 == int(stock_name) :
-                    print(f'{i} {stock_number}주 매도할게요.')
+        print(self.money)
 
-                    ## 매도에 필요한 금액 계산 ##
-                    sell_price = self.stock_list[i] * int(stock_number)
+        while True:
+            name = input("매도할 주식을 입력해주세요: ")
+            if name in self.user_stock:
+                while True:
+                    quantity = input("매도 수량을 입력해주세요: ")
+                    try:
+                        if int(quantity) <= self.user_stock[name]:
 
-                    self.money += sell_price      # 매도액만큼 현금 감소
-                    total_sell += sell_price  # 총 매도액 계산   
-                    print(f"현금이 {self.money:,}원 남았습니다.") 
+                            price = self.stock_list[name]            # 이름으로 입력
+                            sell_price = price * int(quantity)  # 총 매도금액
 
-                    try :
-                        self.user_stock[i] -= int(stock_number)
-                        break
-                    except KeyError :
-                        self.user_stock[i] = int(stock_number)
-                        break
+                            self.user_stock[name] -= int(quantity)
+                            self.money += sell_price         # 매도액만큼 현금 증가
+                            total_sell += sell_price    # 총 매도액 계산
 
-                a += 1
-                question = input('더 구매할까요? 1) 예 2) 아니요 : ')    
-                if question == '1' : continue
-                elif question =='2' : 
-                    print(f"오늘 총 매도금액: {total_sell:,}")
-                    break
-            self.save_stock()    # txt 파일에 저장
-            return self.user_stock, self.money
+                            print(f"오늘 총 매도금액: {total_sell:,}")
+                            break
+
+                        print("보유한 수량보다 더 많이 매도할 수 없습니다.")
+                    except ValueError:
+                        print("매도 수량을 정확히 입력해 주세요.")  
+                break
+            else:
+                print("보유하지 않은 주식입니다.")
+        print(f"{name} {quantity}주를 매도했습니다.")
+
+        self.save_stock()    # txt 파일에 저장
+
+        print(f"지금까지 사용한 금액 : {self.wallet-self.money:,}")   # 이거 보유 주식의 총액(가치가) 아니고 순매수 금액임. 처음 샀을 떄 들어간 원금
+        print(f"보유 현금 : {self.money:,}")
+
+        return self.user_stock, self.money
 
     # 메모장에 보유 주식 저장
     def save_stock(self):
